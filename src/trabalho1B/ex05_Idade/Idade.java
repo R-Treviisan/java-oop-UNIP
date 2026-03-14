a8a3edd26f79f10b33fa78dce49598a5df3eee73
Rodrigo Trevisan <strevisan.r@gmail.com>
2026-03-13 21:36:40 -0300

    chore: Deixando o trabalho de idade mais robusto quase aprova de falhas

--- src/trabalho1B/ex05_Idade/Idade.java
+++ src/trabalho1B/ex05_Idade/Idade.java
@@ -5,14 +5,34 @@
 
 package trabalho1B.ex05_Idade;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Idade {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite sua data de nascimento: ");
+        System.out.print("Digite sua data de nascimento: dd/MM/yyyy: ");
         String dataNascimento = scanner.nextLine();
+
+        // se a data for digitada 210787
+        if (dataNascimento.length() == 6) {
+            dataNascimento = dataNascimento.substring(0, 2) + "/"
+                    + dataNascimento.substring(2, 4) + "/19"
+                    + dataNascimento.substring(4, 6);
+
+            // se a data for digitada 21/07/87
+        } else if (dataNascimento.length() == 8 && dataNascimento.contains("/")) {
+            dataNascimento = dataNascimento.substring(0, 6) + "19"
+                    + dataNascimento.substring(6, 8);
+        }
+        // se a data for digitada 21071987
+        else if (dataNascimento.length() == 8 && !dataNascimento.contains("/")) {
+            dataNascimento = dataNascimento.substring(0, 2) + "/"
+                    + dataNascimento.substring(2, 4) + "/"
+                    + dataNascimento.substring(4, 8);
+        }
+
 
         String[] partes = dataNascimento.split("/");
 
@@ -20,10 +40,15 @@ public class Idade {
         int mes = Integer.parseInt(partes[1]);
         int ano = Integer.parseInt(partes[2]);
 
+        LocalDate agora = LocalDate.now();
+        int idadeDia = agora.getDayOfMonth() - dia;
+        int idadeMes = agora.getMonthValue() - mes;
+        int idadeAno = agora.getYear() - ano;
 
-        int viveuAno = 365 * ano;
-        int viveuMes = 30 * mes;
-        int viveuTotal = viveuMes + viveuAno + dia;
+
+        int viveuAno = 365 * idadeAno;
+        int viveuMes = 30 * idadeMes;
+        int viveuTotal = viveuMes + viveuAno + idadeDia;
 
         System.out.println("Você " + viveuTotal + " Dias");
 
